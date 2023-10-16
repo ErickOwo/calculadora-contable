0196010e95abc8cc2ce8c940e76096580b27dd31
Erick Rodríguez <e12antonio@gmail.com>
2023-10-16 16:40:59 -0600

    something else

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className='flex justify-center w-full p-2 bg-[#8222f2] text-white'>
       <div className='flex flex-wrap justify-center gap-3'>
-        <p>Kendaly Jimena Yax Popa</p>
+        <p>Kéndaly Jimena Yax Popa</p>
         <p>202231965</p>
       </div>
     </header>
